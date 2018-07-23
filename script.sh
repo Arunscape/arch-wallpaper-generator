0e21bc530446dd2bd17044f4e17a15a08e94e06d
Arunscape <arunscape@gmail.com>
2018-07-22 21:40:24 -0600

    holy shit it works but it's slow af probably should've used python pillow lmao

--- script.sh
+++ script.sh
@@ -4,9 +4,12 @@ width=${1:-3840}
 height=${2:-2160}
 logosize=${3:-614}
 
+rm -rf ./wallpapers
+mkdir ./wallpapers
 
 function generateWallpaper(){
   text=$1
+  local count=$2
   #resize arch logo
   convert -background none -size $logosize Archlinux-icon-crystal-64.svg tmplogo.png
 
@@ -14,9 +17,14 @@ function generateWallpaper(){
   convert -size ${width}x${height} canvas:\#111111 tmplogo.png \
     -gravity center -composite \
     -font Courier -pointsize $((logosize/5)) -fill \#eeeeee -annotate +0+$logosize "$text" -gravity center \
-    out.png
+    $count.png
 
   rm tmplogo.png
+  mv $count.png ./wallpapers/$count.png
 }
 
-generateWallpaper "btw I use Arch"
+count=0
+while read -r l; do
+  generateWallpaper "$l" $count
+  ((count++))
+done <lines.txt
