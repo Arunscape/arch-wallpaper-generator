76efecb6e6a11f996e145ef2dcda6bf64340e01b
Arunscape <arunscape@gmail.com>
2018-07-22 21:21:18 -0600

    refactor into function

--- script.sh
+++ script.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-width=$1
-height=$2
-logosize=$3
-text=$4
-
-#resize arch logo
-
-convert -background none -size $logosize Archlinux-icon-crystal-64.svg tmplogo.png
+width=${1:-3840}
+height=${2:-2160}
+logosize=${3:-614}
 
 
+function generateWallpaper(){
+  text=$1
+  #resize arch logo
+  convert -background none -size $logosize Archlinux-icon-crystal-64.svg tmplogo.png
 
-convert -size ${width}x${height} canvas:\#111111 tmplogo.png \
-  -gravity center -composite \
-  -font Courier -pointsize $((logosize/5)) -fill \#eeeeee -annotate +0+$logosize "$4" -gravity center \
-  out.png
+  #output with caption
+  convert -size ${width}x${height} canvas:\#111111 tmplogo.png \
+    -gravity center -composite \
+    -font Courier -pointsize $((logosize/5)) -fill \#eeeeee -annotate +0+$logosize "$text" -gravity center \
+    out.png
 
+  rm tmplogo.png
+}
 
-rm tmplogo.png
+generateWallpaper "btw I use Arch"
